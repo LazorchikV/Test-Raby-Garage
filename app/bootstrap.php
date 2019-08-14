cc3bb7fdfb03460ee3f88fb02b1931ac6ab5074d
LazorchikV <lazorchik29081989@gmail.com>
2019-08-14 23:16:24 +0300

    Test-Raby-Garage

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,5 +3,4 @@
 session_start();
 
 require_once 'const.php';
-//require_once '../vendor/autoload.php';
-require_once '../view/templates/main.php';
+require_once '../vendor/autoload.php';
--- app/config/database.php
+++ app/config/database.php
@@ -1,16 +1,18 @@
 <?php
 
 if (strpos($_SERVER['SERVER_NAME'], 'herokuapp') !== false) {
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
     $host = $url["host"];
     $user = $url["user"];
     $pass = $url["pass"];
     $db = substr($url["path"],1);
 } else {
     $host = DB_HOST;
-    $user = DB_USER;
-    $pass = DB_PASS;
+    $user = 'user'; //DB_USER;
+    $pass = '123';  //DB_PASS;
     $db = DB_NAME;
 }
 
 return mysqli_connect($host, $user, $pass, $db);
+}
+	
