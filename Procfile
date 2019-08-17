fd4f5fcab9be15e369962e841ddad5aab2393117
LazorchikV <lazorchik29081989@gmail.com>
2019-08-17 13:02:52 +0300

    Test-Raby-Garage

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: vendor/bin/heroku-php-apache2 public/
+web: cd ./deploy/tomcat/bin && export 'JAVA_OPTS=-Dport.http=$PORT' && ./catalina.sh run
--- app/const.php
+++ app/const.php
@@ -7,11 +7,13 @@ define('TEMPLATE_PATH', BASE_PATH . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEP
 define('CONFIG_PATH', BASE_PATH . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR);
 
 // Local DB
-define('DB_HOST', 'ec2-54-247-85-251.eu-west-1.compute.amazonaws.com');
-define('DB_NAME', 'dbh3t61an3o44v');
-define('DB_USER', 'nxupoquycwegjb');
-define('DB_PASS', '2bf84765ab908b399b17439c235a3332d41835739f28db7eb44a239b9ed4ce08');
+define('DB_HOST', 'ec2-79-125-2-142.eu-west-1.compute.amazonaws.com');
+define('DB_NAME', 'd88tp9kb9fssp3');
+define('DB_USER', 'yprlawxccbmsle');
+define('DB_PASS', '1cff1f67292d2e4248be52abc25fa9853ca8e34de9ee951914ff755ceba744d8');
 define('DB_PORT', '5432');
+define('DB_URI', 'postgres://yprlawxccbmsle:1cff1f67292d2e4248be52abc25fa9853ca8e34de9ee951914ff755ceba744d8@ec2-79-125-2-142.eu-west-1.compute.amazonaws.com:5432/d88tp9kb9fssp3');
+define('DB_HEROKU_CLI', 'heroku pg:psql postgresql-cubic-11634 --app ubytest');
 
 // Messages
 define('MSG_ERROR', 'danger');
