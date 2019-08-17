6ae4d99af66703cfb01a1ac6cc58276d51254b81
LazorchikV <lazorchik29081989@gmail.com>
2019-08-17 11:28:29 +0300

    Test-Raby-Garage

--- app/const.php
+++ app/const.php
@@ -7,10 +7,11 @@ define('TEMPLATE_PATH', BASE_PATH . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEP
 define('CONFIG_PATH', BASE_PATH . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR);
 
 // Local DB
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'task_list');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'ec2-54-247-85-251.eu-west-1.compute.amazonaws.com');
+define('DB_NAME', 'dbh3t61an3o44v');
+define('DB_USER', 'nxupoquycwegjb');
+define('DB_PASS', '2bf84765ab908b399b17439c235a3332d41835739f28db7eb44a239b9ed4ce08');
+define('DB_PORT', '5432');
 
 // Messages
 define('MSG_ERROR', 'danger');
