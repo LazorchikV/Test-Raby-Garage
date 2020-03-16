fb996a20b6c66d1db6403ae5bb988adfe219cc1b
LazorchikV <lazorchik29081989@gmail.com>
2020-03-16 20:31:15 +0200

    database init

--- app/config/database.php
+++ app/config/database.php
@@ -1,5 +1,5 @@
 <?php
-if (strpos($_SERVER['SERVER_NAME'], 'herokuapp') !== false) {
+/* if (strpos($_SERVER['SERVER_NAME'], 'herokuapp') !== false) {
     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
     $host = $url["host"];
     $user = $url["user"];
@@ -12,3 +12,21 @@ if (strpos($_SERVER['SERVER_NAME'], 'herokuapp') !== false) {
     $db = DB_NAME;
 }
 return mysqli_connect($host, $user, $pass, $db);
+
+
+
+@@ -1,16 +1,14 @@
+<?php */
+if (strpos($_SERVER['SERVER_NAME'], 'herokuapp') !== false) {
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL=>mysql://bf421ffcc254ec:4ff4c4e4@eu-cdbr-west-02.cleardb.net/heroku_8463be4e64122bf?reconnect=true"));
+    $host = $url["host"];
+    $user = $url["user"];
+    $pass = $url["pass"];
+    $db = substr($url["path"],1);
+} else {
+    $host = DB_HOST;
+    $user = DB_USER;
+    $pass = DB_PASS;
+    $db = DB_NAME;
+}
+return mysqli_connect($host, $user, $pass, $db); 
